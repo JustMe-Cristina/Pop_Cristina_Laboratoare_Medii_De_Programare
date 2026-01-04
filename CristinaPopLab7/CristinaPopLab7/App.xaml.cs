2d3c5a3104202c040b28b723c65713d7f2b546aa
JustMe-Cristina <cristinapop809@gail.com>
2026-01-04 16:22:50 +0200

    init clean

---
+++ CristinaPopLab7/CristinaPopLab7/App.xaml.cs
@@ -0,0 +1,11 @@
+﻿namespace CristinaPopLab7;
+
+public partial class App : Application
+{
+    public App()
+    {
+        InitializeComponent();
+
+        MainPage = new AppShell();
+    }
+}
---
+++ CristinaPopLab7/CristinaPopLab7/AppShell.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace CristinaPopLab7;
+
+public partial class AppShell : Shell
+{
+    public AppShell()
+    {
+        InitializeComponent();
+    }
+}
